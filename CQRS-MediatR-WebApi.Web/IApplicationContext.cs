ce8cdbcaaecd42819b04f2bf7744c0865a01ecd0
Jean Lopes <jean.llopes@outlook.com>
2020-05-25 02:01:48 -0300

    Add project files.

---
+++ CQRS-MediatR-WebApi.Web/IApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using CQRS_MediatR_WebApi.Web.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace CQRS_MediatR_WebApi.Web
+{
+    public interface IApplicationContext
+    {
+        DbSet<Product> Products { get; set; }
+
+        Task<int> SaveChanges();
+    }
+}
